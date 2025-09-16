a9a2105c493ac661024ab7259cf02f6c628af6d4
Camilla Soares Sousa <camillasoares818@gmail.com>
2025-09-16 10:26:22 -0300

    feat: Adicionar cena de pré-carregamento e imagens de frutas

--- src/game/scenes/PreloaderScene.ts
+++ src/game/scenes/PreloaderScene.ts
@@ -9,30 +9,29 @@ export class Preloader extends Scene
 
     init ()
     {
-        //  We loaded this image in our Boot Scene, so we can display it here
-        this.add.image(512, 384, 'background');
-
-        //  A simple progress bar. This is the outline of the bar.
         this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+        const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
 
-        //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(512-230, 384, 4, 28, 0xffffff);
-
-        //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
         this.load.on('progress', (progress: number) => {
-
-            //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
             bar.width = 4 + (460 * progress);
-
         });
     }
 
     preload ()
     {
-        //  Load the assets for the game - Replace with your own assets
         this.load.setPath('assets');
 
-        this.load.image('logo', 'logo.png');
+        this.load.image('logo_brawler', 'logo-healthy-brawler.png');
+        this.load.image('apple', './fruits/apple.png');
+        this.load.image('banana', './fruits/banana.png');
+        this.load.image('carrot', './fruits/carrot.png');
+        this.load.image('grape', './fruits/grape.png');
+        this.load.image('pineapple', './fruits/pineapple.png');
+        this.load.image('strawberry', './fruits/strawberry.png');
+        this.load.image('watermelon', './fruits/watermelon.png');
+        this.load.image('tomato', './fruits/tomato.png');
+        this.load.image('corn', './fruits/corn.png');
+        this.load.image('lime', './fruits/lime.png');
     }
 
     create ()
@@ -41,6 +40,6 @@ export class Preloader extends Scene
         //  For example, you can define global animations here, so we can use them in other scenes.
 
         //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
-        this.scene.start('MainMenu');
+        this.scene.start('SplashScreen');
     }
 }
